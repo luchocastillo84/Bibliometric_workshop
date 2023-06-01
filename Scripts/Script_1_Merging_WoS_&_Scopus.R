1d7a8888300045f62a342890f78e413cbb45c619
Luis Carlos Castillo Tellez <65243923+luchocastillo84@users.noreply.github.com>
2023-06-01 17:54:24 +0200

    update program and presentation

--- Scripts/Script_1_Merging_WoS_&_Scopus.R
+++ Scripts/Script_1_Merging_WoS_&_Scopus.R
@@ -19,139 +19,127 @@ Biblio_col <- read_excel(here("Data", # Load the necessary column to conduct
                               "Tags", # Bibliometric analysis
                               "colnames_M.xlsx")) #
 
-col_names_M <- Biblio_col$`Var name` # extract columns name
+col_names_M <- Biblio_col$`Var name` # extract columns name as a vector
+
+
 
 ################################################################################
 ################################### Web of Science #############################
 ################################################################################
 
 # Creating a vector with thre files downloaded by the Web of Science platform
-wos_file <- c("wos_1_1000_30052023.txt", "wos_1001_2000_30052023.txt",
-              "wos_2001_2964_30052023.txt")
+wos_file <- c("wos_1_1000_01062023.bib", "wos_1001_2000_01062023.bib",
+              "wos_2001_2984_01062023.bib")
 
 # Converting the data into a data frame readable in the bibliometrix package and
 # Biblioshiy application
 
-raw_W <- convert2df(here("Data", # Keeping the origianl data in a seperate DF
-                         "Raw", 
-                         wos_file), 
-                    dbsource = "wos", 
-                    format = "plaintext")
-
-class(raw_W)
-
-W <- convert2df(here("Data", # This is a copy of the raw data to modify
+W <- convert2df(here("Data", # Load your Web of Science data and save it to W
                      "Raw", 
                      wos_file), 
                 dbsource = "wos", 
-                format = "plaintext")
+                format = "bibtex")
 
-# this code bring the country of each author into the column AU_CO
-W <- metaTagExtraction(W, 
-                       Field = "AU_CO", 
-                       sep = ";")
+class(W) # checking the type of data frame
 
-PF<- rep("wos", nrow(W)) # PF means platform to distinguish docs from scopus
+PF_wos<- rep("wos", nrow(W)) # PF means platform to distinguish docs from scopus and dimensions
 
-W <- cbind(W, PF) # binding the column to W
-
- 
-# http://www.bibliometrix.org/vignettes/Data-Importing-and-Converting.html
-# The reduction was from 74 columns to 29 
-
-W <- W[ , c(col_names_M)]
-
-# This will add International Standart Book Number ISBN to DOI column 
-W$DI <- ifelse(is.na(W$DI), W$BN, W$DI) 
+W <- cbind(W, PF_wos) # binding the vector PF to M makes M to lose the attributes for biblioshiny
 
 
-colnames(W) # check the column names
-
-vis_miss(W) #  visualize missing values 
-
-W <-  W %>% distinct(DI, .keep_all = T) # keeping only the unique docs by DI or DOI
-
+class(W) # checking the type of data frame
 
 ################################################################################
 ################################### Scopus #####################################
 ################################################################################
 
-raw_S <- convert2df(here("Data", # raw data frame
-                         "Raw", 
-                         "sco_1_1405_30052023.csv"),
-                    dbsource = "scopus", 
-                    format = "csv")
-
-S <- convert2df(here("Data",  # processed data frame for bibliometrics 36 columns
+S <- convert2df(here("Data", # Load your Scopus data and save it to S
                      "Raw", 
-                     "sco_1_1405_30052023.csv"),
+                     "sco_1_1405_01062023.bib"),
                 dbsource = "scopus", 
-                format = "csv")
+                format = "bibtex")
 
-# this code bring the country of each author into the column AU_CO 37 columns
-S <- metaTagExtraction(S, 
-                       Field = "AU_CO", 
-                       sep = ";")
+class(S) # checking the type of data frame
 
-S <- metaTagExtraction(S, 
-                       Field = "AU_UN", 
-                       sep = ";")
+PF_sco<- rep("sco", nrow(S)) # PF means platform to distinguish docs from scopus and dimensions 39 columns
+S <- cbind(S, PF_sco) # binding the vector PF to M makes M to lose the attributes for biblioshiny
 
-vis_miss(S)
+class(S) # checking the type of data frame
 
-S <- S %>% replace_with_na_all(condition = ~. == "") # converting "" into NA within the df
-S <- S %>% replace_with_na_all(condition = ~. == "NA") # converting "" into NA within the df
-
-vis_miss(S)
-
-S <- S %>% mutate("SC" = "", # add research areas (empty column) 
-                  "WC" = "", # add WoS categories (empty column)
-                  "Z9" = "") %>% # add Total Times Cited Count (empty column) total 42 columns
-  rename("TI" = "TI", # renaming title according to WoS tags
-         "ID" = "ID",# keywords
-         "PU" = "PU", 
-         "BN" = "URL")
-
-PF<- rep("sco", nrow(S)) # PF means platform to distinguish docs from scopus and dimensions 39 columns
-S <- cbind(S, PF) # binding the vector PF to M makes M to lose the attributes for biblioshiny
-
-S <- S[, col_names_M] # match tag and number of columns from the Wos 29 columns
-
-S <- S %>% replace_with_na_all(condition = ~. == "") # converting "" into NA within the S
-
-S <-  S %>% distinct(DI, .keep_all = T) # keeping only the unique docs by DI or DOI
-rownames(S) <- S$SR # using SR as row names
-vis_miss(S)
 
 ################################################################################
 ################################### Merging the Data ###########################
 ################################################################################
 
 
-SW <- anti_join(S, W, by= c("DI", "TI")) # filtering docs that only appear in SCOPUS and not WOS 
+# M is the name of the df that merge WOS and SCO 
+M <- mergeDbSources(S, W, remove.duplicated = TRUE)
 
-# M is the name of the df that (M) binds WOS and SCO 
-M <-  rbind(W, SW) # binding the three data bases into B
+
+# This code extracts the country from where the authors are
+M <- metaTagExtraction(M, 
+                        Field = "AU_CO", 
+                        sep = ";")
+
+
+# Identify which data base are the publications in M 
+
+M$PF <- ifelse(M$DI %in% S$DI, "sco", ifelse(M$DI %in% W$DI, "wos", NA))
+
+# This will select only the necessary columns to conduct bibliometric analysis
+M <- M[,col_names_M]
+
+
+# This orders your df in chornological order
+M = M[order(M$PY), ]
+
+vis_miss(M) # Visualize M
+
 M <- M %>% replace_with_na_all(condition = ~. == "") # converting "" into NA within the D
 M <- M %>% replace_with_na_all(condition = ~. == "NA") # converting "" into NA within the D
-vis_miss(M)
+
+# This will add the ISBN to the DOI column 
+M$DI <- ifelse(is.na(M$DI), M$BN, M$DI)
+
+vis_miss(M) # Visualize M
 
 
-
+# Deleting duplicates 
 M <-  M %>% distinct(DI, .keep_all = T) # keeping only the unique docs by DOI
 M <-  M %>% distinct(TI, .keep_all = T) # keeping only the unique docs by TI title
 M <-  M %>% distinct(SR, .keep_all = T) # keeping only the unique docs by SR title
 
-# If you want to filter years manualy
-M <- M %>% arrange(PY) %>% filter(PY >= 2000 & PY< 2023) # excluding 2023
+# If you want to filter years manually
+# M <- M %>% arrange(PY) %>% filter(PY >= 2000 & PY< 2023) # excluding 2023
 
 
-indices <- which(apply(M[, c("TI", "DE")], 1, # filters only TI, DE with the pattern
+# If you want to refine your search and include only the publications 
+# that contains the word artificial intelligence in the TI and DE
+indices <- which(apply(M[, c("TI", "DE")], 1, # filters only TI, DE with the word
                        function(x) any(grepl("artificial intelligence", 
-                                             x, ignore.case = TRUE)))) #  
+                                             x, ignore.case = TRUE)))) # 
 
+# This code select the documents that has Artificial intelligence word in the TI and DE
 M <- M[indices, ]
 
+
+# Perhaps you want to check also what are publications that do not contain AI in the TI
+# indices <- which(!(grepl("artificial intelligence", 
+#                          M$TI, ignore.case = TRUE) | grepl("artificial intelligence", 
+#                                                            M$DE, ignore.case = TRUE)))
+
+
+# indices <- which(apply(M[, c("TI")], 1, # filters only TI, DE with the word
+#                        function(x) any(grepl("artificialintelligence", 
+#                                              x, ignore.case = TRUE)))) # 
+# 
+# M$TI[indices] <- gsub("ARTIFICIALINTELLIGENCE", "ARTIFICIAL INTELLIGENCE ", M$TI[indices])
+
+
+
+
+
+# If you want to save your data frame you can save it in CSV and then convert ti in EXCEL
 write_csv(M, file = here("Data", 
                          "Processed", 
                          "M_AI.csv")) # writing as CSV to make readable in biblioshiny
@@ -163,7 +151,10 @@ class(M) <- c("bibliometrixDB", "data.frame")
 
 class(M)
 
-
+# This will save you data in format rda which is readable in R and biblioshiny
 save(M, file = here("Data", 
                     "Processed", 
                     "M_AI.rda"))
+
+
+
